d520e327c2741871115454213aee619ff0333ee0
noname <git.ubuntu@gmail.com>
2024-01-01 17:47:01 +0500

    add many2one field and add date and datetime type fields

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -4,5 +4,8 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread','mail.activity.mixin']
     _description = "Hospital Appointment"
-
-   
+    
+    
+    patient_id = fields.Many2one('hospital.patient', string='Patient')
+    appointment_time = fields.Datetime(string='Appointment Time')
+    booking_date = fields.Date(string='Booking Date')
