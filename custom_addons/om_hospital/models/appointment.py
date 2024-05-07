941186216d203170bbc127afc93d03d2a253f7b5
noname <git.ubuntu@gmail.com>
2024-05-07 16:21:37 +0500

    add computed filed

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -1,11 +1,21 @@
 from odoo import api, fields, models
 
+
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
-    _inherit = ['mail.thread','mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
-    
-    
+
     patient_id = fields.Many2one('hospital.patient', string='Patient')
-    appointment_time = fields.Datetime(string='Appointment Time')
-    booking_date = fields.Date(string='Booking Date')
+
+    # if we want to get default values in any field then use default="anything",
+    # for date we use default=fields.Date.context_today and for datetime we use default=fields.Datetime.now
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+
+    # if we want to create related field then we use related attribute(by default related fields are readonly but if
+    # we want to be add then add attribute readonly=False but changes in this field also change value ofits parentmodel)
+    # gender = fields.Selection([('male', 'Male'), ('female',
+    # 'Female')], string="Gender", related='patient_id.gender') (both are working)
+    gender = fields.Selection(related='patient_id.gender')
+
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,3 +1,4 @@
+from datetime import date
 from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
@@ -5,10 +6,28 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread','mail.activity.mixin']
     _description = "Hospital Patient"
 
-    name = fields.Char(string='Name', tracking=True) # if we want to tracke any field data then place tracking=true
-    age = fields.Integer(string='Age')
+    # if we want to track any field data then place tracking=true
+    name = fields.Char(string='Name', tracking=True)
+    # this age field is just normal integer filed but it can be make as computed field
+    # age = fields.Integer(string='Age')
+    # Note : computed field can't be set in filter or group_by because they can't save in
+    # database by default it can be stored by set attribute of stored = True
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
+    dob = fields.Date(string='Date Of Birth')
     ref = fields.Char(string='Reference')
 
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender") #this field is used for multiple selections
-    
-    active = fields.Boolean(string="Active", default=True) # creation of this field is mendatory for active archive button
+    # this field is used for multiple selections
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+
+    # creation of this field is mendatory for active archive button
+    active = fields.Boolean(string="Active", default=True)
+
+    @api.depends('dob')
+    def _compute_age(self):
+        # iterate record one by for preventing singleton error
+        for rec in self:
+            if rec.dob:
+                today = date.today()
+                rec.age = today.year - rec.dob.year
+            else:
+                rec.age = 0
